0b363e1688579db3b8413ea6fe6477520b7dee7f
roger rodriguez <rogerdavid.rd@gmail.com>
2023-10-03 22:42:48 -0300

    modifica version node

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,29 @@ dotenv.config();
 import pkg from "pg";
 const {Pool} = pkg;
 
+const connectionString = process.env.PG_URL;
 
+// export const pool = new Pool({
+//     allowExitOnIdle: true
+// });
 
-export const pool = new Pool({allowExitOnIdle: true});
+export const pool = connectionString
+    ? new Pool(
+        {
+        connectionString,
+        ssl: {
+        rejectUnauthorized:false
+    },
+    allowExitOnIdle:true,
+    }
+):
+new Pool({
+    allowExitOnIdle: true
+});
 
+try {
+    await pool.query("SELECT NOW()");
+    console.log("database connected")
+} catch (error) {
+    console.log(error);
+}
